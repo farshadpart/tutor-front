dfa85a46aac2b962903557af6e684e6333ad555f
Farshad Shahsavari <f.sh.ss89@hotmail.com>
2025-07-12 12:08:20 +0930

    Refactor: Move API key to a separate, gitignored file

--- services/chatGptService.ts
+++ services/chatGptService.ts
@@ -1,7 +1,7 @@
 import OpenAI from "openai";
 import { ChatCompletion } from "openai/resources/index.mjs";
-const apiKey = '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';
-const client = new OpenAI({apiKey, dangerouslyAllowBrowser: true});
+import { KEY } from "./keyProvider";
+const client = new OpenAI({apiKey: KEY, dangerouslyAllowBrowser: true});
 
 export interface Chat {
     role: 'user' | 'assistant' | 'system';
