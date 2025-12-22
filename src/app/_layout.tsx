5bc275ceb05adacd0c316cd3a4040e7259041894
farshadpart <f.sh.ss89@gmail.com>
2025-12-22 11:34:32 +1030

    Fix the white flash bug

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -2,6 +2,7 @@ import { Stack } from 'expo-router';
 import { useAuthStore } from '../hooks/useAuthStore';
 import { useTheme, ThemeProvider } from '@/src/providers/ThemeProvider';
 import { ThemedModalProvider } from '../components/themedModal/ThemedModalContext';
+import { View } from 'react-native';
 
 function ThemedStack() {
     const authStore = useAuthStore();
@@ -9,36 +10,38 @@ function ThemedStack() {
 
     return (
         <ThemedModalProvider>
-            <Stack
-                screenOptions={{
-                    headerStyle: {
-                        backgroundColor: theme.colors.surface,
-                    },
-                    headerTintColor: theme.colors.text,
-                    headerTitleStyle: {
-                        color: theme.colors.text,
-                    },
-                    contentStyle: {
-                        backgroundColor: theme.colors.background,
-                    },
-                    statusBarStyle: scheme === 'dark' ? 'light' : 'dark',
-                    animation: scheme === 'dark' ? 'fade' : 'default',
-                }}
-            >
-                <Stack.Protected guard={authStore.user === undefined}>
-                    <Stack.Screen name="index" options={{ headerShown: false }} />
-                </Stack.Protected>
-                <Stack.Protected guard={authStore.user !== undefined && authStore.user.subscriptionGroup !== undefined}>
-                    <Stack.Screen name="chatArea" options={{ headerShown: false }} />
-                </Stack.Protected>
-                <Stack.Protected guard={authStore.user === undefined}>
-                    <Stack.Screen name="Login" />
-                    <Stack.Screen name="Register" />
-                </Stack.Protected>
-                <Stack.Protected guard={authStore.user !== undefined && authStore.user.subscriptionGroup === undefined}>
-                    <Stack.Screen name="Subscription" />
-                </Stack.Protected>
-            </Stack>
+            <View style={{flex: 1, backgroundColor: theme.colors.background}}>
+                <Stack
+                    screenOptions={{
+                        headerStyle: {
+                            backgroundColor: theme.colors.surface,
+                        },
+                        headerTintColor: theme.colors.text,
+                        headerTitleStyle: {
+                            color: theme.colors.text,
+                        },
+                        contentStyle: {
+                            backgroundColor: theme.colors.background,
+                        },
+                        statusBarStyle: scheme === 'dark' ? 'light' : 'dark',
+                        animation: scheme === 'dark' ? 'fade' : 'default',
+                    }}
+                >
+                    <Stack.Protected guard={authStore.user === undefined}>
+                        <Stack.Screen name="index" options={{ headerShown: false }} />
+                    </Stack.Protected>
+                    <Stack.Protected guard={authStore.user !== undefined && authStore.user.subscriptionGroup !== undefined}>
+                        <Stack.Screen name="chatArea" options={{ headerShown: false }} />
+                    </Stack.Protected>
+                    <Stack.Protected guard={authStore.user === undefined}>
+                        <Stack.Screen name="Login" />
+                        <Stack.Screen name="Register" />
+                    </Stack.Protected>
+                    <Stack.Protected guard={authStore.user !== undefined && authStore.user.subscriptionGroup === undefined}>
+                        <Stack.Screen name="Subscription" />
+                    </Stack.Protected>
+                </Stack>
+            </View>
         </ThemedModalProvider>
     );
 }
