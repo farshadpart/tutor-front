299d7d1bf0430e0e3ab91e663442775b0010c61f
farshadpart <f.sh.ss89@gmail.com>
2026-03-09 15:31:23 +1030

    #67 Support loading indicator in the button component

--- src/components/themedTouchableOpacity/ThemedTouchableOpacity.tsx
+++ src/components/themedTouchableOpacity/ThemedTouchableOpacity.tsx
@@ -1,12 +1,16 @@
-import { TouchableOpacity, ViewStyle } from 'react-native';
-import { useTheme } from '@/src/providers/ThemeProvider';
 import { ThemedTouchableOpacityProps } from '@/src/components/themedTouchableOpacity/types/ThemedTouchableOpacityProps';
+import { useTheme } from '@/src/providers/ThemeProvider';
+import { useState } from 'react';
+import { ActivityIndicator, GestureResponderEvent, TouchableOpacity, ViewStyle } from 'react-native';
 
 export function ThemedTouchableOpacity({
+    children,
+    onPress,
     style,
     activeOpacity = 0.7,
     ...rest
 }: ThemedTouchableOpacityProps) {
+    const [loading, setLoading] = useState(false);
     const { theme } = useTheme();
 
     const defaultStyle: ViewStyle = {
@@ -14,11 +18,25 @@ export function ThemedTouchableOpacity({
         borderRadius: 8,
     };
 
+    const handlePress = async (e: GestureResponderEvent) => {
+        setLoading(true);
+        if (!onPress) {
+            setLoading(false);
+            return;
+        }
+
+        await onPress(e);
+        setLoading(false);
+    }
+
     return (
         <TouchableOpacity
             {...rest}
+            disabled={loading}
             activeOpacity={activeOpacity}
             style={[defaultStyle, style]}
-        />
+            onPress={handlePress}>
+            {loading ? <ActivityIndicator color={theme.colors.text} /> : children }
+        </TouchableOpacity>
     );
 }
