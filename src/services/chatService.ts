5e4bbfce75e5c12551400d649c8079c16f790e71
Farshad Shahsavari <f.sh.ss89@gmail.com>
2026-06-30 20:51:09 +0930

    #103 Send the whole chat history to the GPT model

--- src/services/chatService.ts
+++
@@ -1,9 +0,0 @@
-import { Chat } from "@/src/types/chat/chat";
-
-export const makeChatReady = (conversation: Chat[], input: string): Chat[] => {
-    const lastMessages = conversation.slice(-6);
-    lastMessages.push({ role: 'user', content: input });
-
-    return lastMessages;
-}
-
